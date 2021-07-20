4473e775a6c21330e63362238c8ecc4c500a0d8c
spencerkasper <spener.kasper@gmail.com>
2021-07-20 00:45:24 -0500

    * Some debug for update removed bands

--- update-removed-bands/updateRemovedBandsLambdaHandler.js
+++ update-removed-bands/updateRemovedBandsLambdaHandler.js
@@ -1,4 +1,6 @@
 exports.handler = async (event, context) => {
+    console.error('Here is the body');
+    console.error(event.body);
     const removedBands = JSON.parse(event.body);
     console.error(removedBands);
     return {statusCode: 200, body: removedBands};
