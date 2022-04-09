1ce661e3a3301e391b020433a5307b8815b28629
spencerkasper <spener.kasper@gmail.com>
2022-04-09 14:57:06 -0500

    * Add some additional logging

--- save-schedule/saveScheduleLambdaHandler.js
+++ save-schedule/saveScheduleLambdaHandler.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 
 const {CompetitionMessageParser} = require("../shared-message-parsers/competition-message-parser");
 const S3Client = require('../s3Client').S3Client;
-const s3Client = new S3Client();
 
 exports.handler = async function (event, context) {
     try {
         const competition = new CompetitionMessageParser(event).getCompetition();
         console.error(`Saving to competition: ${competition}`);
         const schedule = event.body;
+        console.error(JSON.stringify(schedule));
+        const s3Client = new S3Client();
         await s3Client.put(
             s3Client.createPutPublicJsonRequest(
                 'bitter-jester-lake',
@@ -18,6 +19,7 @@ exports.handler = async function (event, context) {
         )
         return {responseCode: 200, body: {schedule}};
     } catch (e) {
+        console.error(e);
         return e;
     }
 };
