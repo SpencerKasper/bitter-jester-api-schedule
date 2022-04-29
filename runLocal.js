87917935268d447bbab09ff147249955633d0894
spencerkasper <spener.kasper@gmail.com>
2022-04-28 20:41:52 -0500

    * Try to update the saved schedule when the removed bands are changed

--- runLocal.js
+++ runLocal.js
@@ -1,7 +1,8 @@
-const {handler} = require('./get-schedule/getScheduleLambdaHandler');
+const {handler} = require('./update-removed-bands/updateRemovedBandsLambdaHandler');
 
 const run = async () => {
-    const response = await handler({competitionId: 'bitter_jester_summer_2022'});
+    const removedBands = ["MTN AIR","Bed Heads, The","Alpha The Artist","D-Wedgies","F.L.A.M.E.","Funk For Food","Laikaa","Luscia Jane","Moon Rules Apply ","Polly On the Wall","Recovery, The","Root Cause, The","Rotting King","SPARKY","Scumbag Skippy","Sugar Weather","Voodoo Dolls, The","Nobodys","Geology Club","No Shelter"];
+    const response = await handler({body: JSON.stringify({removedBands}), competitionId: 'bitter_jester_summer_2022'});
     console.error(response);
 }
 
--- update-removed-bands/updateRemovedBandsLambdaHandler.js
+++ update-removed-bands/updateRemovedBandsLambdaHandler.js
@@ -5,12 +5,40 @@ exports.handler = async (event, context) => {
     const competition = new CompetitionMessageParser(event).getCompetition();
     const request = JSON.parse(event.body);
 
-    if(!request.removedBands || !Array.isArray(request.removedBands)){
-        return {statusCode: 400, body: {message: 'Bad Request: "removedBands" node must be provided in body as an array of strings.'}};
+    if (!request.removedBands || !Array.isArray(request.removedBands)) {
+        return {
+            statusCode: 400,
+            body: {message: 'Bad Request: "removedBands" node must be provided in body as an array of strings.'}
+        };
     }
 
     const s3Client = new S3Client();
-    if(request.removedBands){
+    if (request.removedBands) {
+        const response = await s3Client.getObject('bitter-jester-lake', `${competition}/removed-bands.json`);
+        const completedSubmissions = await s3Client.getObject('bitter-jester-lake', `${competition}/completed-submissions.json`);
+        const removedBandsBefore = response && response.removedBands ? response.removedBands : [];
+        const bandsToAddBack = removedBandsBefore.filter(band => !request.removedBands.includes(band));
+        const bandsToRemove = request.removedBands.filter(band => !removedBandsBefore.includes(band));
+        console.error(bandsToAddBack);
+        console.error(bandsToRemove);
+        const appsToAddBack = completedSubmissions.completedApplications.filter(app => bandsToAddBack.includes(app.bandName));
+        const schedule = await s3Client
+            .getObject('bitter-jester-lake', `${competition}/user-friday-night-schedule.json`);
+        const updatedNights = schedule.nights.map(night => {
+            const bandsForNightWithSomeRemoved = night.bands.filter(band => !bandsToRemove.includes(band.bandName));
+            return {
+                ...night,
+                bands: bandsForNightWithSomeRemoved,
+            }
+        });
+        updatedNights[0].bands.unshift(appsToAddBack);
+        await s3Client.put(
+            s3Client.createPutPublicJsonRequest(
+                'bitter-jester-lake',
+                `${competition}/user-friday-night-schedule.json`,
+                JSON.stringify({...schedule, nights: updatedNights}),
+            )
+        )
         await s3Client.put(s3Client.createPutPublicJsonRequest(
             'bitter-jester-lake',
             `${competition}/removed-bands.json`,
