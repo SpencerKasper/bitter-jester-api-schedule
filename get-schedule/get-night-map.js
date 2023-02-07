d7226aaa2623dcce4a7027b8d33c8faea8f88884
spencerkasper <spener.kasper@gmail.com>
2023-02-07 13:51:34 -0600

    * Update the night map too

--- get-schedule/get-night-map.js
+++ get-schedule/get-night-map.js
@@ -6,6 +6,13 @@ exports.getNightMap = function (competitionId) {
             3: '6',
             4: '13'
         };
+    } else if(competitionId === 'bitter_jester_summer_2023') {
+      return {
+          1: '2',
+          2: '3',
+          3: '9',
+          4: '10',
+      }
     } else {
         return {
             1: '20',
